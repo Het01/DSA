12d7eb68933342072878dedbec15e8653e7f4797
Het Patel <106577127+Het01@users.noreply.github.com>
2023-06-15 22:49:56 +0530

    Update Pattern-1.cpp

--- Patterns/Pattern-1.cpp
+++ Patterns/Pattern-1.cpp
@@ -1,3 +1,5 @@
+class Solution {
+  public:
     void printSquare(int n) {
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
@@ -6,3 +8,4 @@
             cout<<endl;
         }
     }
+};
