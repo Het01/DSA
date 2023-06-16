0812efc9be092daa7caec9aab28421434730e00e
Het Patel <106577127+Het01@users.noreply.github.com>
2023-06-15 21:09:35 -0700

    Update Implement_stack_using_array.cpp

--- Stack/Implement_stack_using_array.cpp
+++ Stack/Implement_stack_using_array.cpp
@@ -1,1 +1,31 @@
+/*
+class MyStack
+{
+private:
+    int arr[1000];
+    int top;
+public:
+    MyStack(){top=-1;}
+    int pop();
+    void push(int);
+};
+*/
 
+void MyStack :: push(int x)
+{
+    if(top==999){
+        return;
+    }
+    top++;
+    arr[top]=x;
+}
+
+//Function to remove an item from top of the stack.
+int MyStack :: pop()
+{
+    if(top==-1){
+        return -1;
+    }       
+    // top--;
+    return arr[top--]; 
+}
