99dc72f99f5b019d9241ce30922fc733f77cbaf3
Het Patel <106577127+Het01@users.noreply.github.com>
2023-06-17 22:35:57 +0530

    Delete Bit_Manipulation  directory

--- Bit_Manipulation /Intro.cpp
+++
@@ -1,7 +0,0 @@
-class Solution {
-  public:
-    void bitManipulation(int num, int i) {
-        int mask=1<<(i-1);
-        cout<< bool(num & mask) <<" "<<(num | mask)<<" "<<(~mask & num);
-    }
-};
